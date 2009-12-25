980808dd650e87985cf764813f710d53c5b05c3b
Lawrence Grant <karbon@gmail.com>
2009-12-25 04:00:31 +0000

    Addition of PrimaryKey funtionality...

--- src/DataColumnAttribute.cs
+++ src/DataColumnAttribute.cs
@@ -5,6 +5,13 @@ namespace ooglue
 {
 	public class DataColumnAttribute : DataAttribute
 	{
+		public bool IsPrimaryKey { get; set; }
+		
 		public DataColumnAttribute(string name) : base(name) { }
+		
+		public DataColumnAttribute(string name, bool isPrimaryKey) : this(name)
+		{
+			this.IsPrimaryKey = isPrimaryKey;
+		}
 	}
 }
