9e23cfee359b5fbbcd1e72f3fe400c1d3b6491aa
Lawrence Grant <karbon@gmail.com>
2009-12-25 03:59:32 +0000

    Cleaning up the exchange code for easier usage.

--- src/DataExchange.cs
+++ src/DataExchange.cs
@@ -1,10 +1,13 @@
 
 using System;
 using System.Data;
+using System.Data.Common;
 using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+
+using ooglue.Access;
 
 namespace ooglue
 {
@@ -14,9 +17,14 @@ namespace ooglue
 		{
 		}
 		
-		public static T GetFromDataReader<T>(IDataReader reader) where T : new()
+		public static T GetObjectFromDataReader<T>(IDataReader reader) where T : new()
 		{
 			return GetFromDataReader<T>(reader, 1)[0];
+		}
+		
+		public static List<T> GetFromDataReader<T>(IDataReader reader) where T : new()
+		{
+			return GetFromDataReader<T>(reader, int.MaxValue);
 		}
 		
 		public static List<T> GetFromDataReader<T>(IDataReader reader, int count) where T : new()
@@ -35,18 +43,28 @@ namespace ooglue
 				}
 			}
 			
-			while(reader.Read() && counter++ < count)
+			while(reader.Read() && count != 0 && counter++ < count)
 			{
 				T t = new T();
 				string fieldName = string.Empty;
 				for(int i = 0; i < reader.FieldCount; i++)
 				{
+					try
+					{
+					if(reader.IsDBNull(i))
+						continue;
+					
 					fieldName = reader.GetName(i);
 					if(propertyMap.ContainsKey(fieldName))
 					{
-						//assign the fucking property value.
 						PropertyInfo info = propertyMap[fieldName];
+						System.Console.WriteLine("Mapping {0} to {1})", reader.GetValue(i).ToString(), info.Name);
 						info.SetValue(t, reader.GetValue(i), null);
+					}
+					}
+					catch(Exception ex)
+					{
+						Console.WriteLine("Error:" + ex.ToString());
 					}
 				}
 				returnList.Add(t);
@@ -55,7 +73,71 @@ namespace ooglue
 			return returnList;
 		}
 		
-		public static T GetObjectFromMethod<T>(DataAccess access, MethodInfo method) where T : new()
+		public static IDbCommand GetCommandFromObject<T>(DataAccess access, T paramsObject)
+		{
+			IDbConnection connection = access.NewConnection;
+			IDbCommand command = connection.CreateCommand();
+			
+			foreach(PropertyInfo propertyInfo in typeof(T).GetProperties())
+			{				
+				List<DataColumnAttribute> columnAttributes = 
+					new List<DataColumnAttribute>((DataColumnAttribute[])propertyInfo.GetCustomAttributes(typeof(DataColumnAttribute), true));
+				if(columnAttributes.Count > 0)
+				{
+					IDbDataParameter parameter = command.CreateParameter();
+					parameter.ParameterName = access.ParameterPrefix + propertyInfo.Name;
+					parameter.Value = propertyInfo.GetValue(paramsObject, null);
+					command.Parameters.Add(parameter);
+				}
+			}
+			
+			return command;
+		}
+		
+		public static IDbCommand GetDynamicInsertFromObject<T>(DataAccess access, T paramsObject)
+		{
+			string templateString = "insert into {0} ({1}) values ({2})";
+			string valueTemplateString = "'{0}'";
+			string dataTableName = string.Empty;
+			List<string> fields = new List<string>();
+			List<string> values = new List<string>();
+			List<DataTableAttribute> dataTableAttributes =
+				new List<DataTableAttribute>((DataTableAttribute[])typeof(T).GetCustomAttributes(typeof(DataTableAttribute), true));
+			
+			IDbConnection connection = access.NewConnection;
+			IDbCommand command = connection.CreateCommand();
+			
+			if(dataTableAttributes.Count > 0)
+			{
+				dataTableName = dataTableAttributes[0].Name;
+			}
+			foreach(PropertyInfo propertyInfo in typeof(T).GetProperties())
+			{				
+				List<DataColumnAttribute> columnAttributes = 
+					new List<DataColumnAttribute>((DataColumnAttribute[])propertyInfo.GetCustomAttributes(typeof(DataColumnAttribute), true));
+				if(columnAttributes.Count > 0)
+				{
+					fields.Add(columnAttributes[0].Name);
+					values.Add(string.Format(valueTemplateString,propertyInfo.GetValue(paramsObject, null)));
+				}
+			}
+			
+			command.CommandText = string.Format(
+			                                    templateString, 
+			                                    dataTableName, 
+			                                    string.Join(",", fields.ToArray()), 
+			                                    string.Join(",", values.ToArray()));
+			
+			command.CommandType = CommandType.Text;
+			
+			return command;
+		}
+		
+		
+		/*
+		 * This may not be the right thing to do in this class. This probably isn't the place to interact with SQL
+		 * 
+		public static T GetObjectFromMethod<T>(DataAccess access, MethodBase method) where T : new()
 		{
 			List<DataMethodAttribute> methodAttributes = new List<DataMethodAttribute>((DataMethodAttribute[])method.GetCustomAttributes(typeof(DataMethodAttribute), true));
 			foreach(DataMethodAttribute dma in methodAttributes)
@@ -86,20 +168,24 @@ namespace ooglue
 			}
 			return default(T);
 		}
-					
-		private static IDataParameter[] GetDataParametersFromInfo(ParameterInfo [] parameters)
+		*/
+		
+		/*
+		private static Dictionary<string, object> GetInputParametersFromInfo(ParameterInfo [] parameters)
 		{
-			List<IDataParameter> returnParams = new List<IDataParameter>();
+			Dictionary<string, object> returnParams = new Dictionary<string, object>();
 				
 			foreach(ParameterInfo parameterInfo in parameters)
 			{
 				List<InAttribute> ins = new List<InAttribute>((InAttribute[])parameterInfo.GetCustomAttributes(typeof(InAttribute), true));
+				
 				foreach(InAttribute ina in ins)
 				{
-				
+					returnParams.Add(ina.Name, parameterInfo.
 				}
 			}
-			return returnParams.ToArray();
+			return returnParams;
 		}
+		*/
 	}
 }
