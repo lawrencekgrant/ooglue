d7213708d155342cfcd212c4f8210d78566a3c6e
Lawrence Grant <karbon@gmail.com>
2009-12-26 22:29:28 +0000

    Componentizing and adding Select.

--- src/DataExchange.cs
+++ src/DataExchange.cs
@@ -1,11 +1,13 @@
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 
 using ooglue.Access;
 
@@ -101,8 +103,8 @@ namespace ooglue
 		
 		public static IDbCommand GetDynamicInsertFromObject<T>(DataAccess access, T paramsObject)
 		{
-			string templateString = "insert into {0} ({1}) values ({2})";
-			string valueTemplateString = "'{0}'";
+			const string templateString = "insert into {0} ({1}) values ({2})";
+			const string valueTemplateString = "'{0}'";
 			string dataTableName = string.Empty;
 			List<string> fields = new List<string>();
 			List<string> values = new List<string>();
@@ -118,18 +120,13 @@ namespace ooglue
 			}
 			foreach(PropertyInfo propertyInfo in typeof(T).GetProperties())
 			{	
-				List<KeyFieldAttribute> keyFields =
-					new List<KeyFieldAttribute>((KeyFieldAttribute[])propertyInfo.GetCustomAttributes(typeof(KeyFieldAttribute), true));
-				
-				if(keyFields.Count > 0)
-				{
-					continue;
-				}
-				
 				List<ColumnAttribute> columnAttributes = 
 					new List<ColumnAttribute>((ColumnAttribute[])propertyInfo.GetCustomAttributes(typeof(ColumnAttribute), true));
 				if(columnAttributes.Count > 0)
 				{
+					if(columnAttributes[0].IsPrimaryKey)
+						continue;
+					
 					fields.Add(columnAttributes[0].Name);
 					values.Add(string.Format(valueTemplateString,propertyInfo.GetValue(paramsObject, null)));
 				}
@@ -149,50 +146,41 @@ namespace ooglue
 		
 		public static IDbCommand GetDynamicUpdateFromObject<T>(DataAccess access, T paramsObject)
 		{
-			string templateString = "update {0} set {1} where {2} = {3}";
-			string valueTemplateString = "{0} = '{1}'";
-			string dataTableName = string.Empty;
-			string primaryKeyId = string.Empty;
-			string primaryKeyValue = string.Empty;
-			List<TableAttribute> dataTableAttributes =
-				new List<TableAttribute>((TableAttribute[])typeof(T).GetCustomAttributes(typeof(TableAttribute), true));
+			const string templateString = "update {0} set {1} where {2} = {3}";
+			const string valueTemplateString = "{0} = '{1}'";
 			
+			string dataTableName = string.Empty;
+			KeyValuePair<string,string> primaryKey = new KeyValuePair<string, string>();
 			List<string> updateValues = new List<string>();
 			
-			IDbConnection connection = access.NewConnection;
-			IDbCommand command = connection.CreateCommand();
-			
-			if(dataTableAttributes.Count > 0)
-			{
-				dataTableName = dataTableAttributes[0].Name;
-			}
+			dataTableName = getTableNameFromObject<T>(paramsObject);
 			
 			foreach(PropertyInfo propertyInfo in typeof(T).GetProperties())
-			{	
-				List<KeyFieldAttribute> keyFields =
-					new List<KeyFieldAttribute>((KeyFieldAttribute[])propertyInfo.GetCustomAttributes(typeof(KeyFieldAttribute), true));
-				
-				if(keyFields.Count > 0)
-				{
-					primaryKeyId = keyFields[0].Name;
-					primaryKeyValue = propertyInfo.GetValue(paramsObject, null).ToString();
-					continue;
-				}
-				
+			{					
 				List<ColumnAttribute> columnAttributes = 
 					new List<ColumnAttribute>((ColumnAttribute[])propertyInfo.GetCustomAttributes(typeof(ColumnAttribute), true));
 				if(columnAttributes.Count > 0)
 				{
-					updateValues.Add(string.Format(valueTemplateString, columnAttributes[0].Name, propertyInfo.GetValue(paramsObject, null)));
+					ColumnAttribute column = columnAttributes[0];
+					if(column.IsPrimaryKey)
+					{
+						primaryKey = new KeyValuePair<string, string>(column.Name, propertyInfo.GetValue(paramsObject, null).ToString());
+						continue;
+					}
+					else
+					{
+						updateValues.Add(string.Format(valueTemplateString, columnAttributes[0].Name, propertyInfo.GetValue(paramsObject, null)));
+					}
 				}
 			}
 			
+			IDbCommand command = access.NewConnection.CreateCommand ();
 			command.CommandText = string.Format(
 			                                    templateString, 
 			                                    dataTableName, 
 			                                    string.Join(",", updateValues.ToArray()),
-			                                    primaryKeyId,
-			                                    primaryKeyValue);
+			                                    primaryKey.Key,
+			                                    primaryKey.Value);
 			
 			command.CommandType = CommandType.Text;
 			
@@ -201,41 +189,104 @@ namespace ooglue
 		
 		public static IDbCommand GetDynamicDeleteFromObject<T>(DataAccess access, T paramsObject)
 		{
-			string commandTextTemplate = "delete from {0} where {1} = '{2}'";
-			string primaryKeyId = string.Empty;
-			string primaryKeyValue = string.Empty;
-			string tableName = string.Empty;
+			const string commandTextTemplate = "delete from {0} where {1} = '{2}'";
+			
+			string tableName;
+			KeyValuePair<string,string> primaryKey;
 			IDbConnection connection = access.NewConnection;
 			IDbCommand command = connection.CreateCommand();
+			
+			tableName = getTableNameFromObject<T>(paramsObject);
+			primaryKey = getPrimaryKeyFromObject<T>(paramsObject);
+			
+			command.CommandText = string.Format(
+			                                    commandTextTemplate,
+			                                    tableName,
+			                                    primaryKey.Key,
+			                                    primaryKey.Value);
+			command.CommandType = CommandType.Text;
+			return command;
+		}
+		
+		public static IDbCommand GetDynamicSelectFromObject<T>(DataAccess access, T paramsObject)
+		{
+			const string commandText = "select {0} from {1} {2}";
+			string dataTableName = getTableNameFromObject<T>(paramsObject);
+			IDbCommand command = access.NewConnection.CreateCommand();
+			command.CommandText = string.Format(
+			                                    commandText,
+			                                    getFieldListFromObject<T>(paramsObject),
+			                                    dataTableName,
+			                                    getWhereListFromObject<T>(paramsObject));
+			command.CommandType = CommandType.Text;
+			
+			return command;
+		}
+		
+		internal static string getFieldListFromObject<T>(T searchObject)
+		{
+			StringBuilder builder = new StringBuilder();
+			
+			var items = from pinfo in typeof(T).GetProperties() 
+				from attr in (ColumnAttribute[]) pinfo.GetCustomAttributes(typeof(ColumnAttribute), true)
+				select attr.Name;
+			
+			return string.Join(" , ", items.ToArray());
+		}
+		
+		internal static string getWhereListFromObject<T>(T searchObject)
+		{
+			StringBuilder builder = new StringBuilder();
+			
+			var items = from pinfo in typeof(T).GetProperties()
+				from attr in (ColumnAttribute[]) pinfo.GetCustomAttributes(typeof(ColumnAttribute), true)
+				where (    pinfo.GetValue(searchObject, null) != null && 
+					       pinfo.GetValue(searchObject, null).ToString() != Activator.CreateInstance(pinfo.PropertyType).ToString()
+					       )
+				select string.Format("{0} = '{1}'", attr.Name, pinfo.GetValue(searchObject, null).ToString());
+			
+			
+			if(items.Count() > 0)
+				return string.Format("where {0}", string.Join(" and ", items.ToArray()));
+			else
+				return string.Empty;
+		}
+		
+		internal static KeyValuePair<string, string> getPrimaryKeyFromObject<T>(T searchObject)
+		{
+			KeyValuePair<string,string> primaryKey = new KeyValuePair<string, string>();
+			
+			foreach(PropertyInfo propertyInfo in typeof(T).GetProperties())
+			{	
+				List<ColumnAttribute> columnAttributes = 
+					new List<ColumnAttribute>((ColumnAttribute[])propertyInfo.GetCustomAttributes(typeof(ColumnAttribute), true));
+				
+				if(columnAttributes.Count > 0)
+				{
+					ColumnAttribute column = columnAttributes[0];
+					if(column.IsPrimaryKey)
+					{
+						primaryKey = new KeyValuePair<string, string>(column.Name, propertyInfo.GetValue(searchObject, null).ToString());
+						break;
+					}
+				}
+			}
+			
+			return primaryKey;
+		}
+		
+		internal static string getTableNameFromObject<T>(T searchObject)
+		{
+			string dataTableName = string.Empty;
 			
 			List<TableAttribute> dataTables =
 				new List<TableAttribute>((TableAttribute[])typeof(T).GetCustomAttributes(typeof(TableAttribute), true));
 			
 			if(dataTables.Count > 0)
 			{
-				tableName = dataTables[0].Name;
+				dataTableName = dataTables[0].Name;
 			}
-			
-			foreach(PropertyInfo propertyInfo in typeof(T).GetProperties())
-			{	
-				List<KeyFieldAttribute> keyFields =
-					new List<KeyFieldAttribute>((KeyFieldAttribute[])propertyInfo.GetCustomAttributes(typeof(KeyFieldAttribute), true));
-				
-				if(keyFields.Count > 0)
-				{
-					primaryKeyId = keyFields[0].Name;
-					primaryKeyValue = propertyInfo.GetValue(paramsObject, null).ToString();
-					break;
-				}
-			}
-			
-			command.CommandText = string.Format(
-			                                    commandTextTemplate,
-			                                    tableName,
-			                                    primaryKeyId,
-			                                    primaryKeyValue);
-			command.CommandType = CommandType.Text;
-			return command;
+			return dataTableName;
 		}
 		
 		
