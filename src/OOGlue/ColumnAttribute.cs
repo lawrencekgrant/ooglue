38b96c973bdd90c16ea83e1c0ebd42294c48a924
Lawrence Grant <karbon@gmail.com>
2010-04-25 18:36:29 +0000

    Added XML comments to the ColumnAttribute.

--- src/OOGlue/ColumnAttribute.cs
+++ src/OOGlue/ColumnAttribute.cs
@@ -3,15 +3,40 @@ using System;
 
 namespace ooglue
 {
+	/// <summary>
+	/// This attribute is used to tie property or field to a particular column name.
+	/// </summary>
+	/// <remarks>
+	/// These attributes are used in both data mapping and object mapping.
+	/// </remarks>
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 	public sealed class ColumnAttribute : DataAttribute
 	{
+		/// <summary>
+		/// The ooglue system uses this property to determine whether a property is a primary key column. This is used heavily in selecting
+		/// and updating values.
+		/// </summary>
 		public bool IsPrimaryKey { get; set; }
 
+		/// <summary>
+		/// Constructs a new ColumnAttribute
+		/// </summary>
+		/// <param name="name">
+		/// A <see cref="System.String"/> that represents the name of the column to be bound to.
+		/// </param>
 		public ColumnAttribute (string name) : base(name)
 		{
 		}
 
+		/// <summary>
+		/// Constructs a new ColumnAttribute
+		/// </summary>
+		/// <param name="name">
+		/// A <see cref="System.String"/> that represents the name of the column to be bound to.
+		/// </param>
+		/// <param name="isPrimaryKey">
+		/// A <see cref="System.Boolean"/> that determines whether the specified column is a primary key column or not.
+		/// </param>
 		public ColumnAttribute (string name, bool isPrimaryKey) : this(name)
 		{
 			this.IsPrimaryKey = isPrimaryKey;
